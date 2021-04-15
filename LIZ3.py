f286a94449ded990f879157f1d9d137468647ea9
Munyekiliz <81600858+Munyekiliz@users.noreply.github.com>
2021-04-15 15:28:28 -0400

    Add files via upload

---
+++ LIZ3.py
@@ -0,0 +1,11 @@
+# Enter first and last name:
+names = input("Elizabeth Kagwima")
+print(names)
+print()
+
+# Enter your goals for this class:
+
+print("What are your goals for this class - long term goals?")
+goals = input('What would you like to be....when all is said and done with the class?')
+print("I would like to be a Python computer developer")
+print(goals)
